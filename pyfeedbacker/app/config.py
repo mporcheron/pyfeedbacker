6ebf4c2dafac6c28b36cb83307f577c55a2f6835
Martin Porcheron <martin+git@porcheron.uk>
2021-07-18 17:21:09 +0100

    Config now is in a wrapper to allow easy resetting

--- pyfeedbacker/app/config.py
+++ pyfeedbacker/app/config.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import enum
 import configparser
+import copy
 
 
 
-ini = configparser.ConfigParser()
-ini.read('config.ini')
+class ConfigWrapper:
+    def __init__(self):
+        self.reset()
+
+    def __getitem__(self, key):
+        return self.ini[key]
+
+    def __getattr__(self, attr):
+        return self.ini.__getattribute__(attr)
+
+    def reset(self):
+        self.ini = configparser.ConfigParser()
+        self.ini.read('config.ini')
+
+
+
+ini = ConfigWrapper()
